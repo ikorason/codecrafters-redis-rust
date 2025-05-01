960ba75420d9fecf01e610b7fee12e1b0c3a0ab5
irfan <irfanabliz914@gmail.com>
2025-05-01 16:28:08 -0400

    response to PING

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let mut stream = stream.unwrap();
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
