cb6dfa4cc20c83111ae2d3ee5f4bdc3ab9579a8f
irfan <irfanabliz914@gmail.com>
2025-05-01 21:18:20 -0400

    response to multiple PINGS

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+const RESPONSE: &[u8; 7] = b"+PONG\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("Listening on 127.0.0.1:6379");
 
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        stream.write_all(b"+PONG\r\n").unwrap();
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    loop {
+        let mut buffer = [0; 512];
+        let bytes_read = stream
+            .read(&mut buffer)
+            .expect("Failed to read from stream");
+        if bytes_read == 0 {
+            // client closed the connection
+            break;
+        }
+
+        // Optional: inspect input
+        // println!("Received: {:?}", &buffer[..bytes_read]);
+
+        stream.write_all(RESPONSE).unwrap();
     }
 }
